41aa5ee2841c8e4523fa9ceabeb0ca9d1b014e73
alexander.holmstro3 <alle020916@gmail.com>
2020-01-31 11:17:21 +0100

    2020-01-31 sluta av lektion

--- src/Komplexatal.java
+++ src/Komplexatal.java
@@ -1,4 +1,15 @@
+/*
+Alexander holmström
+
+*/
 public class Komplexatal {
     private double a;
     private double b;
+
+    public Komplexatal(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+
 }
